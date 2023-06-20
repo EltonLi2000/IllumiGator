7c8ba3c66195d074403651f5c56b3fa8d886e9ee
Dakota Cappel <dakota.cappel@ufl.edu>
2023-06-20 12:35:16 -0400

    Adding possible dojo map for consideration

---
+++ design/galar dojo tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="galar dojo tiles" tilewidth="16" tileheight="16" tilecount="2244" columns="44">
+ <image source="galar dojo tiles.png" width="704" height="816"/>
+</tileset>
---
+++ design/pokemon-gba-world-map-editor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="pokemon-gba-world-map-editor" tilewidth="16" tileheight="16" tilecount="13348" columns="94">
+ <image source="pokemon-gba-world-map-editor.png" width="1504" height="2273"/>
+</tileset>
